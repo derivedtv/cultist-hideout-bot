cff32a1aff004a7318b8086ab794d70858a89526
derivedtv <38233444+derivedtv@users.noreply.github.com>
2018-04-10 22:27:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -178,10 +178,25 @@ setTimeout(function(){
 snekfetch.get(rapi).then(r => {
   let rdesc = r.body.description;
   let rname = r.body.name
+  let rstars = r.body.rank
+  let rcount = r.body.characterCount
+  let rlocation = r.body.last_seen
+  let rfame = r.body.fame
 
   if(!rdesc.includes(rcode))
   return message.author.send("Your code was not found in the first line of your Realmeye description. Your previous Realmeye description was:\n```" + brdesc + "```")
 
+  if(!rstars >= 30)
+  return message.author.send("You do not have enough stars to be verified! You have " + rstars + ". You need __**30**__.")
+
+  if(!rcount >= 2)
+  return message.author.send("You do not have enough characters to be verified!")
+
+  if(!rlocation.includes("hidden"))
+  return message.author.send("Your location is not hidden so you cannot be verified!")
+
+  if(!rfame >= 1500)
+  return message.author.send("Your do not have enough fame to be verified! You have " + rfame + ". You need __**1500**__.")
   if(rdesc.includes(rcode))
   message.guild.member(message.author).setNickname(`${rname}`)
   message.guild.member(message.author).addRole("407245309232807936")
